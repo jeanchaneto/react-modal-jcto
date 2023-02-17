8726d55ad159e3a7ed93f97d70450c98377f43a2
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2023-02-17 09:14:40 +0100

    Edit doc

--- src/react-modal-jcto.jsx
+++ src/react-modal-jcto.jsx
@@ -1,6 +1,16 @@
 import "./modal.css"
 import React from 'react';
 
+/**
+ * Creates a customizable Modal
+ * @param {modalBackgroundStyle} inline styling for modal background
+ * @param {modalBodyStyle} inline styling for modal body
+ * @param {modalButtonStyle} inline styling for modal button
+ * @param {closeModalFunction} function to close modal
+ * @param {modalTextStyle} inline styling for modal text
+ * @param {modalText} string for modal text
+ * @returns displays a Modal
+ */
 const Modal = ({ modalBackgroundStyle, modalBodyStyle, modalButtonStyle, closeModalFunction, modalTextStyle, modalText }) => {
     return (
         <div className="modal"
